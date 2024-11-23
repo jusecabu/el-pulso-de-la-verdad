60abcf7ac921c36bd0b1571dc33e86a0115b187c
Juan Sebastian Castañeda Burbano <js.castanedaburbano@gmail.com>
2024-11-23 16:13:29 -0500

    minor change

--- src/components/ArticleForm.tsx
+++ src/components/ArticleForm.tsx
@@ -80,7 +80,7 @@ export default function ArticleForm({ onSubmit }: ArticleFormProps) {
         type="submit"
         className="w-full bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
-        Submit Article
+        Subir noticia
       </button>
     </form>
   );
